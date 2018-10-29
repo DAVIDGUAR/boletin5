305250302e6c7dc0ec4e7390b463ada3da8ec96b
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2018-10-29 09:52:09 +0100

    modi

--- src/boletin5/Boletin5.java
+++ src/boletin5/Boletin5.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin5;
 
 /**
@@ -11,11 +6,19 @@ package boletin5;
  */
 public class Boletin5 {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        Conta contaOrigen = new Conta();
+        Conta contaDestino = new Conta();
+        
+        
+        
+        System.out.println("nome=" +nome+n/"nConta=" +nConta+n/"saldo=" +saldo);
+        
     
+        saldoConta.transferencia(1500);
+        System.out.println("transferencia="+ saldoConta.getsaldo());
+    
+}
 }
